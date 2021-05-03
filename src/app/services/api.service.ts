ba6edc14725fa987327dc80040384e6ab0c86ec3
totodore <prevottheodore@gmail.com>
2021-05-03 02:36:42 +0100

    login and logout methods

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,11 +1,42 @@
+import { environment } from './../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Profile } from 'passport-discord';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  public profile?: Profile;
 
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  public async login(token?: string) {
+    this.profile = await this.get<Profile>("user/me", token || this.token);
+    if (token)
+      localStorage.setItem("jwt", token);
+  }
+  public async logout() {
+    this.profile = undefined;
+    localStorage.removeItem("jwt");
+  }
+
+  private async get<R>(path: string, token?: string) {
+    if (path.startsWith("/"))
+      path = path.substring(1);
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token || this.token}` });
+    return this.http.get<R>(`${environment.apiLink}/${path}`, { headers }).toPromise();
+  }
+
+  private get token(): string {
+    return localStorage.getItem("jwt") as string;
+  }
+
+  public get logged(): boolean {
+    return this.token !== null;
+  }
   
 }
