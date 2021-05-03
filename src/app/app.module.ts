f8f9b0868701d92df811ec38779711c57d917f12
totodore <prevottheodore@gmail.com>
2021-05-03 02:36:14 +0100

    adding style and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,49 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthComponent } from './components/auth/auth.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { appearance } from './style/default';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatIconModule } from "@angular/material/icon";
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatInputModule } from "@angular/material/input";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatTabsModule } from "@angular/material/tabs";
+import { NavbarComponent } from './components/dashboard/navbar/navbar.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { GuildBoardComponent } from './components/dashboard/guild-board/guild-board.component';
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent,
+    GuildBoardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatTooltipModule,
+    DragDropModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: appearance },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/style/default.ts
@@ -0,0 +1,7 @@
+import { MatFormFieldDefaultOptions } from "@angular/material/form-field";
+
+const appearance: MatFormFieldDefaultOptions = {
+  appearance: "outline"
+}
+
+export { appearance };
