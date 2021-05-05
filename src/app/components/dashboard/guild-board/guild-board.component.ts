a7f73ef2a39593506c800bb1e65049f37e8365a2
totodore <prevottheodore@gmail.com>
2021-05-06 00:47:23 +0100

    guild board

--- src/app/components/dashboard/guild-board/guild-board.component.ts
+++ src/app/components/dashboard/guild-board/guild-board.component.ts
@@ -1,5 +1,8 @@
+import { GuildReqModel } from './../../../models/api.model';
+import { ApiService } from 'src/app/services/api.service';
 import { ActivatedRoute } from '@angular/router';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { DiscordGuild } from 'src/app/models/api.model';
 
 @Component({
   selector: 'app-guild-board',
@@ -9,13 +12,21 @@ import { Component, Input, OnInit } from '@angular/core';
 export class GuildBoardComponent implements OnInit {
 
   public guildId: string | null = null;
-  
+  public discordGuild?: DiscordGuild;
+  public guild?: GuildReqModel;
+
   constructor(
-    private readonly route: ActivatedRoute
+    private readonly route: ActivatedRoute,
+    private readonly api: ApiService
   ) { }
 
   public ngOnInit(): void {
-    this.route.paramMap.subscribe(e => this.guildId = e.get("id"));
+    this.route.paramMap.subscribe(async e => {
+      this.guildId = e.get("id")
+      if (this.guildId) {
+        this.discordGuild = this.api.profile?.guilds.find(el => el.id === this.guildId);
+        this.guild = await this.api.getGuild(this.guildId);
+      }
+    });
   }
-
 }
