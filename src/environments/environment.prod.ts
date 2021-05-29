a9a9f81110fc17da5422a424c00a92b672d2ca10
totodore <prevottheodore@gmail.com>
2021-05-29 19:17:39 +0100

    patching new links with new bot

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   discordCdn: "https://cdn.discordapp.com",
   apiLink: "https://api.automatebot.app",
-  botLink: "https://discord.com/api/oauth2/authorize?client_id=714141847035052051&permissions=8&redirect_uri=https%3A%2F%2Fbeta.automatebot.app%2Fguild%2Fauth&scope=bot&disable_guild_select=true&guild_id=",
-  oauthLink: "https://discordapp.com/api/oauth2/authorize?client_id=714141847035052051&redirect_uri=https%3A%2F%2Fbeta.automatebot.app%2Foauth&response_type=code&scope=identify%20guilds"
+  botLink: "https://discord.com/api/oauth2/authorize?client_id=848230396973285428&permissions=8&redirect_uri=https%3A%2F%2Fbeta.automatebot.app%2Fguild%2Fauth&scope=bot&disable_guild_select=true&guild_id=",
+  oauthLink: "https://discordapp.com/api/oauth2/authorize?client_id=848230396973285428&redirect_uri=https%3A%2F%2Fbeta.automatebot.app%2Foauth&response_type=code&scope=identify%20guilds"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   discordCdn: "https://cdn.discordapp.com",
   apiLink: "http://localhost:3000",
-  botLink: "https://discord.com/api/oauth2/authorize?client_id=714141847035052051&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fguild%2Fauth&scope=bot&disable_guild_select=true&guild_id=",
-  oauthLink: "https://discord.com/api/oauth2/authorize?client_id=714141847035052051&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fuser%2Fauth&response_type=code&scope=identify%20guilds"
+  botLink: "https://discord.com/api/oauth2/authorize?client_id=848230396973285428&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fguild%2Fauth&scope=bot&disable_guild_select=true&guild_id=",
+  oauthLink: "https://discord.com/api/oauth2/authorize?client_id=848230396973285428&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fuser%2Fauth&response_type=code&scope=identify%20guilds"
 };
 
 /*
