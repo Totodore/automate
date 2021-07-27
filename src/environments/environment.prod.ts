71d6f2ef9c28e4978113ea51001073bf6f461ee4
Théodore Prévot <prevottheodore@gmail.com>
2021-07-27 15:20:14 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   discordCdn: "https://cdn.discordapp.com",
   apiLink: "https://api.automatebot.app",
-  botLink: "https://discord.com/api/oauth2/authorize?client_id=848230396973285428&permissions=8&redirect_uri=https%3A%2F%2Fapi.automatebot.app%2Fguild%2Fauth&scope=bot&disable_guild_select=true&guild_id=",
-  oauthLink: "https://discord.com/api/oauth2/authorize?client_id=848230396973285428&redirect_uri=https%3A%2F%2Fapi.automatebot.app%2Fuser%2Fauth&response_type=code&scope=identify%20guilds"
+  botLink: "https://discord.com/api/oauth2/authorize?client_id=868885271138152488&permissions=8&redirect_uri=https%3A%2F%2Fapi.automatebot.app%2Fguild%2Fauth&scope=bot&disable_guild_select=true&guild_id=",
+  oauthLink: "https://discord.com/api/oauth2/authorize?client_id=868885271138152488&redirect_uri=https%3A%2F%2Fapi.automatebot.app%2Fuser%2Fauth&response_type=code&scope=identify%20guilds"
 };
