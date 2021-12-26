26e71fa0a476f5376f0bd9baf8f22d75a38c8ab3
Totodore <prevottheodore@gmail.com>
2021-12-27 00:07:04 +0100

    error parse

--- src/app/components/dashboard/guild-board/guild-board.component.ts
+++ src/app/components/dashboard/guild-board/guild-board.component.ts
@@ -4,7 +4,7 @@ import { SnackbarService } from './../../../services/snackbar.service';
 import { environment } from './../../../../environments/environment';
 import { GuildReqModel, MessageModel } from './../../../models/api.model';
 import { ApiService } from 'src/app/services/api.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { DiscordGuild } from 'src/app/models/api.model';
 import { GuildAddMessageComponent } from './guild-add-message/guild-add-message.component';
@@ -32,7 +32,9 @@ export class GuildBoardComponent implements OnInit {
 
   constructor(
     private readonly route: ActivatedRoute,
+    private readonly router: Router,
     private readonly api: ApiService,
+    private readonly snackbar: SnackbarService,
   ) { }
 
   public ngOnInit(): void {
@@ -43,6 +45,12 @@ export class GuildBoardComponent implements OnInit {
         this.discordGuild = this.api.profile?.guilds.find(el => el.id === this.guildId);
         this.guild = await this.api.getGuild(this.guildId);
         this.loading = false;
+      }
+    });
+    this.route.queryParamMap.subscribe(e => {
+      if (e.get("status") === "premium_cancel") {
+        this.snackbar.snack("Ooops ! Premium subscription cancelled", 7000);
+        this.router.navigate(["."], { relativeTo: this.route, queryParams: { status: null } });
       }
     });
   }
