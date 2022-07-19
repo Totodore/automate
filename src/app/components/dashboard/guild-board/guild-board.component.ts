5f1ec3fe467dac9a9e68c7a53ee4972f6703faf7
Théodore Prévot <prevottheodore@gmail.com>
2022-07-19 19:45:55 +0200

    Hotfix: Adding error if guild is null

--- src/app/components/dashboard/guild-board/guild-board.component.ts
+++ src/app/components/dashboard/guild-board/guild-board.component.ts
@@ -4,7 +4,7 @@ import { SnackbarService } from './../../../services/snackbar.service';
 import { environment } from './../../../../environments/environment';
 import { GuildReqModel, MessageModel } from './../../../models/api.model';
 import { ApiService } from 'src/app/services/api.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { DiscordGuild } from 'src/app/models/api.model';
 import { GuildAddMessageComponent } from './guild-add-message/guild-add-message.component';
@@ -34,6 +34,8 @@ export class GuildBoardComponent implements OnInit {
   constructor(
     private readonly route: ActivatedRoute,
     private readonly api: ApiService,
+    private readonly snackback: SnackbarService,
+    private readonly router: Router
   ) { }
 
   public ngOnInit(): void {
@@ -43,6 +45,10 @@ export class GuildBoardComponent implements OnInit {
         this.loading = true;
         this.discordGuild = this.api.profile?.guilds.find(el => el.id === this.guildId);
         this.guild = await this.api.getGuild(this.guildId);
+        if (!this.guild) {
+          this.snackback.snack("Error while getting guild infos, please retry or send a message on the #bug-report channel on Discord");
+          this.router.navigateByUrl("/board");
+        }
         this.isAdmin = checkAdminPermissions(this.discordGuild?.permissions || 0);
         this.loading = false;
       }
